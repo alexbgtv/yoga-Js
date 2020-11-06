a0fa6173551982abf641e61b0a8f95ca420e32f4
bgtvalex <bgtvcxz@mail.ru>
2020-11-06 06:56:36 +0300

    yoga

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// window.addEventListener('DOMContentLoaded', function(){
+
+// 	let tabs = document.querySelector('.info-header'),
+// 		tab = document.querySelectorAll('.info-header-tab'),
+// 		tabContent = document.querySelectorAll('.info-tabcontent')
+
+// 	function hideTabContent(a){
+// 		for (let i = a; i < tab.length; i++) {
+// 			tabContent[i].classList.remove('show')
+// 			tabContent[i].classList.add('hide')
+// 		}
+// 	}
+// 	hideTabContent(1)
+
+// 	function showTabContent(b) {
+// 		if (tabContent[b].classList.contains('hide')) {
+// 			tabContent[b].classList.remove('hide')
+// 			tabContent[b].classList.add('show')
+// 		}
+// 	}
+
+// 	tabs.addEventListener('click', function(event) {
+// 		let target = event.target
+// 		console.log(target)
+// 		if (target && target.classList.contains('info-header-tab')) {
+// 			for (let i = 0; i < tab.length; i++) {
+// 				if (target == tab[i]) {
+// 					hideTabContent(0)
+// 					showTabContent(i)
+// 					break
+// 				}
+// 			}
+// 		}
+// 	})
+// })
+
+
